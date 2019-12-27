9a651b7e7716ed2b32137629e53955b5ee8eb001
Relisiler <demidovnik01@gmail.com>
2019-12-27 09:37:21 +0300

    V1.0

---
+++ client.py
@@ -0,0 +1,40 @@
+import socket
+import pickle
+from random import randint
+
+def combinaton(P1, key):
+    for i in range(len(P1)):
+        P2 = chr(ord(P1[i]) ^ key)
+    return ''.join(P2)
+
+def send_message(sock, message, key):
+    message = combinaton(message, key)
+    sock.send(pickle.dumps(message))
+
+def receive(sock, key):
+    message = pickle.loads(sock.recv(1024))
+    message = combinaton(message, key)
+    return message
+
+HOST = '127.0.0.1'
+PORT = 8080
+
+sock = socket.socket()
+sock.connect((HOST, PORT))
+
+p = randint(0,10000)
+g = randint(0,10000)
+a = randint(0,10000)
+
+A = g ** a % p
+sock.send(pickle.dumps((p, g, A)))
+
+B = pickle.loads(sock.recv(1024))
+key = B ** a % p
+message = input("Сообщение: ")
+while message != 'exit':
+    send_message(sock, message, key)
+    print(receive(sock, key))
+    message = input()
+
+sock.close()
---
+++ server.py
@@ -0,0 +1,43 @@
+import socket
+import pickle
+from random import randint
+
+def combinaton(P1, key):
+    for i in range(len(P1)):
+        P2 = chr(ord(P1[i]) ^ key)
+    return ''.join(P2)
+
+def send_message(sock, message, key):
+    message = combinaton(message, key)
+    sock.send(pickle.dumps(message))
+
+def receive(sock, key):
+    message = pickle.loads(sock.recv(1024))
+    message = combinaton(message,key)
+    return message
+
+HOST = '127.0.0.1'
+PORT = 8080
+
+sock = socket.socket()
+sock.bind((HOST, PORT))
+sock.listen(1)
+conn, addr = sock.accept()
+
+message = conn.recv(1024)
+
+p, g, A = pickle.loads(message)
+b = randint(10, 10000)
+B = g ** b % p
+conn.send(pickle.dumps(B))
+key = A ** b % p
+
+
+while True:
+    try:
+        message = receive(conn, key)
+        print(message)
+        send_message(conn, 'Сообщение успешно получено и расшифровано', key)
+    except EOFError: break
+
+conn.close()
